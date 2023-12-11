c133fe7cdfe2ed256ee5312ac8fc08dedfae034c
Rhômulo Mourão <111456177+RhomuloMourao@users.noreply.github.com>
2023-12-11 18:16:39 -0300

    erro morador corrigido

--- erroglobal.aspx.cs
+++ erroglobal.aspx.cs
@@ -9,6 +9,6 @@ public partial class erroglobal : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        Session.Clear();
+        //Session.Clear();
     }
 }
